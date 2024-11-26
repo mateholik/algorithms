0596e9f185f7a2f62996ac77dfc8ce4ed1b077da
Vladimir <vladimir.vaisov@teltonika.lt>
2024-11-26 18:25:29 +0200

    findCategoryByName helper function approach

--- latest.js
+++ latest.js
@@ -123,16 +123,35 @@ function getAllCategoryNames(tree) {
     return names;
 }
 // console.log(getAllCategoryNames(tree3));
+// function findCategoryByName(
+//   tree: Category3[],
+//   name: string
+// ): Category3 | undefined {
+//   for (let i = 0; i < tree.length; i++) {
+//     if (tree[i].name === name) return tree[i];
+//     if (tree[i].children) {
+//       const result = findCategoryByName(tree[i].children as Category3[], name);
+//       if (result) return result;
+//     }
+//   }
+//   return undefined;
+// }
 function findCategoryByName(tree, name) {
-    for (var i = 0; i < tree.length; i++) {
-        if (tree[i].name === name)
-            return tree[i];
-        if (tree[i].children) {
-            var result = findCategoryByName(tree[i].children, name);
-            if (result)
-                return result;
+    var result = undefined;
+    function traverse(node) {
+        if (node.name === name) {
+            result = node;
+            return;
+        }
+        if (!node.children)
+            return;
+        for (var i = 0; i < node.children.length; i++) {
+            traverse(node.children[i]);
         }
     }
-    return undefined;
+    for (var i = 0; i < tree.length; i++) {
+        traverse(tree[i]);
+    }
+    return result;
 }
 console.log(findCategoryByName(tree3, "category_1_1"));
--- latest.ts
+++ latest.ts
@@ -152,17 +152,44 @@ function getAllCategoryNames(tree: Category3[]): string[] {
 
 // console.log(getAllCategoryNames(tree3));
 
+// function findCategoryByName(
+//   tree: Category3[],
+//   name: string
+// ): Category3 | undefined {
+//   for (let i = 0; i < tree.length; i++) {
+//     if (tree[i].name === name) return tree[i];
+//     if (tree[i].children) {
+//       const result = findCategoryByName(tree[i].children as Category3[], name);
+//       if (result) return result;
+//     }
+//   }
+//   return undefined;
+// }
+
 function findCategoryByName(
   tree: Category3[],
   name: string
 ): Category3 | undefined {
-  for (let i = 0; i < tree.length; i++) {
-    if (tree[i].name === name) return tree[i];
-    if (tree[i].children) {
-      const result = findCategoryByName(tree[i].children as Category3[], name);
-      if (result) return result;
+  let result: Category3 | undefined = undefined;
+
+  function traverse(node: Category3): void {
+    if (node.name === name) {
+      result = node;
+      return;
+    }
+
+    if (!node.children) return;
+
+    for (let i = 0; i < node.children.length; i++) {
+      traverse(node.children[i]);
     }
   }
-  return undefined;
+
+  for (let i = 0; i < tree.length; i++) {
+    traverse(tree[i]);
+  }
+
+  return result;
 }
+
 console.log(findCategoryByName(tree3, "category_1_1"));
