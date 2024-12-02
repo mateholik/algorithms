7afe6b06bce018ca9cee90ea0d89d472e3f2f448
Vladis macbook <vladuxs1@gmail.com>
2024-12-02 23:58:54 +0200

    flattenTree

--- compiled/recursion/flattenTree.js
+++ compiled/recursion/flattenTree.js
@@ -1,3 +1,24 @@
 "use strict";
-// import { tree3 } from "./consts";
-console.log("testas4");
+Object.defineProperty(exports, "__esModule", { value: true });
+const consts_1 = require("./consts");
+const flattenTree = (tree) => {
+    let result = [];
+    const traverse = (node) => {
+        if (typeof node !== "object" ||
+            node === null ||
+            Array.isArray(node) ||
+            Object.keys(node).length === 0)
+            return;
+        result.push({ name: node.name, id: node.id });
+        if (!node.children)
+            return;
+        for (let i = 0; i <= node.children.length; i++) {
+            traverse(node.children[i]);
+        }
+    };
+    for (let i = 0; i <= tree.length; i++) {
+        traverse(tree[i]);
+    }
+    return result;
+};
+console.log(flattenTree(consts_1.tree3));
--- src/recursion/flattenTree.ts
+++ src/recursion/flattenTree.ts
@@ -1,3 +1,30 @@
-// import { tree3 } from "./consts";
+import { tree3, type Category3 } from "./consts";
 
-console.log("testas4");
+const flattenTree = (tree: Category3[]): Category3[] => {
+  let result: Category3[] = [];
+
+  const traverse = (node: Category3): void => {
+    if (
+      typeof node !== "object" ||
+      node === null ||
+      Array.isArray(node) ||
+      Object.keys(node).length === 0
+    )
+      return;
+
+    result.push({ name: node.name, id: node.id });
+    if (!node.children) return;
+
+    for (let i = 0; i <= node.children.length; i++) {
+      traverse(node.children[i]);
+    }
+  };
+
+  for (let i = 0; i <= tree.length; i++) {
+    traverse(tree[i]);
+  }
+
+  return result;
+};
+
+console.log(flattenTree(tree3));
