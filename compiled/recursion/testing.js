627bb5724e1637a03756b76db2bde24888e658e5
Vladis macbook <vladuxs1@gmail.com>
2024-12-15 12:34:34 +0200

    practice

---
+++ compiled/recursion/testing.js
@@ -0,0 +1,187 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteNodeByUniqueId3 = exports.deleteNodeById3 = exports.findNodeById3 = exports.deepCloneTree2 = exports.revertFlatTree3 = exports.flattenTree6 = exports.flattenTree4 = exports.revertFlattenTree2 = exports.revertFlattenTree = exports.getFlattenTree2 = exports.getFlattenTree = void 0;
+const consts_1 = require("./consts");
+const getFlattenTree = (tree, parentId) => {
+    let result = [];
+    for (let i = 0; i < tree.length; i++) {
+        const obj = Object.assign(Object.assign({}, tree[i]), { parentId: parentId || -1 });
+        delete obj.children;
+        result.push(obj);
+        if (tree[i].children) {
+            const children = (0, exports.getFlattenTree)(tree[i].children, tree[i].id);
+            result = [...result, ...children];
+        }
+    }
+    return result;
+};
+exports.getFlattenTree = getFlattenTree;
+// console.log("getFlattenTree", getFlattenTree(tree3));
+const getFlattenTree2 = (tree) => {
+    let result = [];
+    const traverse = (node) => {
+        result.push(node);
+        if (node.children) {
+            for (let i = 0; i < node.children.length; i++) {
+                traverse(node.children[i]);
+            }
+        }
+    };
+    return result;
+};
+exports.getFlattenTree2 = getFlattenTree2;
+// console.log("getFlattenTree2", getFlattenTree(tree3));
+const revertFlattenTree = (flatTree) => {
+    const traverse = (parentId) => {
+        let result = [];
+        for (let i = 0; i < flatTree.length; i++) {
+            if (flatTree[i].parentId === parentId) {
+                result.push(Object.assign(Object.assign({}, flatTree[i]), { children: traverse(flatTree[i].id) }));
+            }
+        }
+        return result;
+    };
+    return traverse(-1);
+};
+exports.revertFlattenTree = revertFlattenTree;
+// console.log(
+//   "revertFlattenTree",
+//   JSON.stringify(revertFlattenTree(getFlattenTree(tree3)), null, 2)
+// );
+const revertFlattenTree2 = (flatTree) => {
+    const traverse = (parentId) => {
+        let result = [];
+        for (let i = 0; i < flatTree.length; i++) {
+            if (flatTree[i].parentId === parentId) {
+                result.push(Object.assign(Object.assign({}, flatTree[i]), { children: traverse(flatTree[i].id) }));
+            }
+        }
+        return result;
+    };
+    return traverse(-1);
+};
+exports.revertFlattenTree2 = revertFlattenTree2;
+const flattenTree4 = (tree) => {
+    let result = [];
+    const traverse = (node, parentId) => {
+        const obj = Object.assign({}, node);
+        delete obj.children;
+        obj.parentId = parentId || -1;
+        result.push(obj);
+        if (node.children) {
+            for (let i = 0; i < node.children.length; i++) {
+                traverse(node.children[i], node.id);
+            }
+        }
+    };
+    for (let i = 0; i < tree.length; i++) {
+        traverse(tree[i]);
+    }
+    return result;
+};
+exports.flattenTree4 = flattenTree4;
+// console.log("flattenTree4", flattenTree4(tree3));
+const flattenTree6 = (tree, parentId = -1) => {
+    let result = [];
+    for (let i = 0; i < tree.length; i++) {
+        const obj = Object.assign({}, tree[i]);
+        delete obj.children;
+        obj.parentId = parentId;
+        result.push(obj);
+        if (tree[i].children) {
+            const children = (0, exports.flattenTree6)(tree[i].children, tree[i].id);
+            result = [...result, ...children];
+        }
+    }
+    return result;
+};
+exports.flattenTree6 = flattenTree6;
+// console.log("flattenTree6", flattenTree6(tree3));
+const revertFlatTree3 = (tree) => {
+    const traverse = (parentId) => {
+        let result = [];
+        for (let i = 0; i < tree.length; i++) {
+            if (tree[i].parentId === parentId) {
+                const node = Object.assign(Object.assign({}, tree[i]), { children: traverse(tree[i].id) });
+                delete node.parentId;
+                result.push(node);
+            }
+        }
+        return result;
+    };
+    return traverse(-1);
+};
+exports.revertFlatTree3 = revertFlatTree3;
+// console.log(
+//   "revertFlatTree3",
+//   JSON.stringify(revertFlatTree3(flattenTree6(tree3)), null, 2)
+// );
+const deepCloneTree2 = (tree) => {
+    let result = [];
+    for (let i = 0; i < tree.length; i++) {
+        result.push(Object.assign(Object.assign({}, tree[i]), { children: tree[i].children
+                ? (0, exports.deepCloneTree2)(tree[i].children)
+                : undefined }));
+    }
+    return result;
+};
+exports.deepCloneTree2 = deepCloneTree2;
+// console.log("deepCloneTree2", JSON.stringify(deepCloneTree2(tree3), null, 2));
+const findNodeById3 = (tree, id) => {
+    for (let i = 0; i < tree.length; i++) {
+        if (tree[i].id === id) {
+            return tree[i];
+        }
+        if (tree[i].children) {
+            const matchInChildren = (0, exports.findNodeById3)(tree[i].children, id);
+            if (matchInChildren)
+                return matchInChildren;
+        }
+    }
+    return undefined;
+};
+exports.findNodeById3 = findNodeById3;
+// console.log(
+//   "findNodeById3",
+//   JSON.stringify(findNodeById3(tree3, 311), null, 2)
+// );
+const deleteNodeById3 = (tree, id) => {
+    let levelResult = [];
+    for (let i = 0; i < tree.length; i++) {
+        if (tree[i].id === id) {
+            continue;
+        }
+        else {
+            levelResult.push(Object.assign(Object.assign({}, tree[i]), { children: tree[i].children
+                    ? (0, exports.deleteNodeById3)(tree[i].children, id)
+                    : undefined }));
+        }
+    }
+    return levelResult;
+};
+exports.deleteNodeById3 = deleteNodeById3;
+// console.log(
+//   "deleteNodeById3",
+//   JSON.stringify(deleteNodeById3(tree3, 3), null, 2)
+// );
+const deleteNodeByUniqueId3 = (tree, id) => {
+    let result = [];
+    for (let i = 0; i < tree.length; i++) {
+        if (tree[i].id === id) {
+            const isUnique = tree[i].linkedCategory
+                ? (0, exports.findNodeById3)(consts_1.tree3, tree[i].linkedCategory) === undefined
+                : true;
+            if (!isUnique) {
+                result.push(Object.assign({}, tree[i]));
+            }
+        }
+        else {
+            result.push(Object.assign(Object.assign({}, tree[i]), { children: tree[i].children
+                    ? (0, exports.deleteNodeByUniqueId3)(tree[i].children, id)
+                    : undefined }));
+        }
+    }
+    return result;
+};
+exports.deleteNodeByUniqueId3 = deleteNodeByUniqueId3;
+console.log("deleteNodeByUniqueId3", JSON.stringify((0, exports.deleteNodeByUniqueId3)(consts_1.tree3, 12), null, 2));
---
+++ src/recursion/testing.ts
@@ -0,0 +1,243 @@
+import { Category3, tree3 } from "./consts";
+
+export const getFlattenTree = (
+  tree: Category3[],
+  parentId?: number
+): Category3[] => {
+  let result: Category3[] = [];
+
+  for (let i = 0; i < tree.length; i++) {
+    const obj = { ...tree[i], parentId: parentId || -1 };
+
+    delete obj.children;
+
+    result.push(obj);
+
+    if (tree[i].children) {
+      const children = getFlattenTree(
+        tree[i].children as Category3[],
+        tree[i].id
+      );
+      result = [...result, ...children];
+    }
+  }
+  return result;
+};
+
+// console.log("getFlattenTree", getFlattenTree(tree3));
+
+export const getFlattenTree2 = (tree: Category3[]): Category3[] => {
+  let result: Category3[] = [];
+
+  const traverse = (node: Category3) => {
+    result.push(node);
+    if (node.children) {
+      for (let i = 0; i < node.children.length; i++) {
+        traverse(node.children[i]);
+      }
+    }
+  };
+
+  return result;
+};
+
+// console.log("getFlattenTree2", getFlattenTree(tree3));
+
+export const revertFlattenTree = (flatTree: Category3[]): Category3[] => {
+  const traverse = (parentId: number) => {
+    let result: Category3[] = [];
+    for (let i = 0; i < flatTree.length; i++) {
+      if (flatTree[i].parentId === parentId) {
+        result.push({ ...flatTree[i], children: traverse(flatTree[i].id) });
+      }
+    }
+    return result;
+  };
+
+  return traverse(-1);
+};
+
+// console.log(
+//   "revertFlattenTree",
+//   JSON.stringify(revertFlattenTree(getFlattenTree(tree3)), null, 2)
+// );
+
+export const revertFlattenTree2 = (flatTree: Category3[]): Category3[] => {
+  const traverse = (parentId: number) => {
+    let result: Category3[] = [];
+    for (let i = 0; i < flatTree.length; i++) {
+      if (flatTree[i].parentId === parentId) {
+        result.push({ ...flatTree[i], children: traverse(flatTree[i].id) });
+      }
+    }
+    return result;
+  };
+  return traverse(-1);
+};
+
+export const flattenTree4 = (tree: Category3[]): Category3[] => {
+  let result: Category3[] = [];
+
+  const traverse = (node: Category3, parentId?: number) => {
+    const obj = { ...node };
+    delete obj.children;
+    obj.parentId = parentId || -1;
+
+    result.push(obj);
+    if (node.children) {
+      for (let i = 0; i < node.children.length; i++) {
+        traverse(node.children[i], node.id);
+      }
+    }
+  };
+  for (let i = 0; i < tree.length; i++) {
+    traverse(tree[i]);
+  }
+  return result;
+};
+// console.log("flattenTree4", flattenTree4(tree3));
+
+export const flattenTree6 = (
+  tree: Category3[],
+  parentId: number = -1
+): Category3[] => {
+  let result: Category3[] = [];
+
+  for (let i = 0; i < tree.length; i++) {
+    const obj = { ...tree[i] };
+    delete obj.children;
+    obj.parentId = parentId;
+    result.push(obj);
+    if (tree[i].children) {
+      const children = flattenTree6(
+        tree[i].children as Category3[],
+        tree[i].id
+      );
+      result = [...result, ...children];
+    }
+  }
+
+  return result;
+};
+
+// console.log("flattenTree6", flattenTree6(tree3));
+
+export const revertFlatTree3 = (tree: Category3[]): Category3[] => {
+  const traverse = (parentId: number) => {
+    let result: Category3[] = [];
+    for (let i = 0; i < tree.length; i++) {
+      if (tree[i].parentId === parentId) {
+        const node = { ...tree[i], children: traverse(tree[i].id) };
+        delete node.parentId;
+        result.push(node);
+      }
+    }
+    return result;
+  };
+
+  return traverse(-1);
+};
+
+// console.log(
+//   "revertFlatTree3",
+//   JSON.stringify(revertFlatTree3(flattenTree6(tree3)), null, 2)
+// );
+
+export const deepCloneTree2 = (tree: Category3[]): Category3[] => {
+  let result: Category3[] = [];
+
+  for (let i = 0; i < tree.length; i++) {
+    result.push({
+      ...tree[i],
+      children: tree[i].children
+        ? deepCloneTree2(tree[i].children as Category3[])
+        : undefined,
+    });
+  }
+
+  return result;
+};
+
+// console.log("deepCloneTree2", JSON.stringify(deepCloneTree2(tree3), null, 2));
+
+export const findNodeById3 = (
+  tree: Category3[],
+  id: number
+): Category3 | undefined => {
+  for (let i = 0; i < tree.length; i++) {
+    if (tree[i].id === id) {
+      return tree[i];
+    }
+    if (tree[i].children) {
+      const matchInChildren = findNodeById3(
+        tree[i].children as Category3[],
+        id
+      );
+      if (matchInChildren) return matchInChildren;
+    }
+  }
+
+  return undefined;
+};
+
+// console.log(
+//   "findNodeById3",
+//   JSON.stringify(findNodeById3(tree3, 311), null, 2)
+// );
+
+export const deleteNodeById3 = (tree: Category3[], id: number): Category3[] => {
+  let levelResult: Category3[] = [];
+
+  for (let i = 0; i < tree.length; i++) {
+    if (tree[i].id === id) {
+      continue;
+    } else {
+      levelResult.push({
+        ...tree[i],
+        children: tree[i].children
+          ? deleteNodeById3(tree[i].children as Category3[], id)
+          : undefined,
+      });
+    }
+  }
+
+  return levelResult;
+};
+
+// console.log(
+//   "deleteNodeById3",
+//   JSON.stringify(deleteNodeById3(tree3, 3), null, 2)
+// );
+
+export const deleteNodeByUniqueId3 = (
+  tree: Category3[],
+  id: number
+): Category3[] => {
+  let result: Category3[] = [];
+
+  for (let i = 0; i < tree.length; i++) {
+    if (tree[i].id === id) {
+      const isUnique = tree[i].linkedCategory
+        ? findNodeById3(tree3, tree[i].linkedCategory as number) === undefined
+        : true;
+
+      if (!isUnique) {
+        result.push({ ...tree[i] });
+      }
+    } else {
+      result.push({
+        ...tree[i],
+        children: tree[i].children
+          ? deleteNodeByUniqueId3(tree[i].children as Category3[], id)
+          : undefined,
+      });
+    }
+  }
+
+  return result;
+};
+
+console.log(
+  "deleteNodeByUniqueId3",
+  JSON.stringify(deleteNodeByUniqueId3(tree3, 12), null, 2)
+);
