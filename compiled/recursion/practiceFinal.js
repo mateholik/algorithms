995943a9f50b274507340e40644485a1e9a68894
Vladis macbook <vladuxs1@gmail.com>
2024-12-17 23:39:03 +0200

    practice

--- compiled/recursion/practiceFinal.js
+++ compiled/recursion/practiceFinal.js
@@ -1,23 +1,156 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteOriginalAndRelatedNodeById = exports.rebuildFlatTree = exports.flattenTree = exports.deleteNodeByUniqueId = exports.findNodeById = void 0;
+exports.revertFlatTree = exports.flattenTree = exports.deleteNodeAndLinkedNodeById = exports.deleteNodeByUniqueId = exports.deleteNodeById = exports.findNodeById = void 0;
 const consts_1 = require("./consts");
+// export const findNodeById = (
+//   tree: Category[],
+//   id: number
+// ): Category | undefined => {
+//   for (const node of tree) {
+//     if (node.id === id) {
+//       return { ...node };
+//     }
+//     if (node.children) {
+//       const matchInChildren = findNodeById(node.children, id);
+//       if (matchInChildren) return matchInChildren;
+//     }
+//   }
+//   return undefined;
+// };
+// // console.log("findNodeById", findNodeById(tree, 311));
+// export const deleteNodeByUniqueId = (
+//   tree: Category[],
+//   id: number,
+//   originalTree: Category[]
+// ): Category[] => {
+//   let updatedTree: Category[] = [];
+//   for (const node of tree) {
+//     if (node.id === id) {
+//       const isUnique = node.linkedCategory
+//         ? findNodeById(originalTree, node.linkedCategory) === undefined
+//         : true;
+//       if (!isUnique) {
+//         updatedTree.push({ ...node });
+//       }
+//     } else {
+//       updatedTree.push({
+//         ...node,
+//         children: node.children
+//           ? deleteNodeByUniqueId(node.children, id, originalTree)
+//           : undefined,
+//       });
+//     }
+//   }
+//   return updatedTree;
+// };
+// // console.log(
+// //   "deleteNodeByUniqueId",
+// //   JSON.stringify(deleteNodeByUniqueId(tree, 3, tree), null, 2)
+// // );
+// export const flattenTree = (
+//   tree: Category[],
+//   parentId?: number
+// ): Category[] => {
+//   let flatTree: Category[] = [];
+//   for (const node of tree) {
+//     const newNode = { ...node };
+//     newNode.parentId = parentId || -1;
+//     delete newNode.children;
+//     flatTree.push(newNode);
+//     if (node.children) {
+//       const flattenChildren = flattenTree(node.children, node.id);
+//       flatTree = [...flatTree, ...flattenChildren];
+//     }
+//   }
+//   return flatTree;
+// };
+// // console.log("flattenTree", flattenTree(tree));
+// export const rebuildFlatTree = (flatTree: Category[]): Category[] => {
+//   const traverse = (parentId: number) => {
+//     let tree: Category[] = [];
+//     for (const node of flatTree) {
+//       if (node.parentId === parentId) {
+//         const newNode = { ...node };
+//         delete newNode.parentId;
+//         newNode.children = traverse(node.id).length
+//           ? traverse(node.id)
+//           : undefined;
+//         tree.push(newNode);
+//       }
+//     }
+//     return tree;
+//   };
+//   return traverse(-1);
+// };
+// // console.log(
+// //   "rebuildFlatTree",
+// //   JSON.stringify(rebuildFlatTree(flattenTree(tree)), null, 2)
+// // );
+// export const deleteOriginalAndRelatedNodeById = (
+//   tree: Category[],
+//   id: number
+// ): Category[] => {
+//   let linkedNode: Category | undefined = undefined;
+//   const traverse = (nodes: Category[]): Category[] => {
+//     let updatedTree: Category[] = [];
+//     for (const node of nodes) {
+//       if (node.id === id) {
+//         linkedNode = node.linkedCategory
+//           ? findNodeById(tree, node.linkedCategory)
+//           : undefined;
+//       } else {
+//         updatedTree.push({
+//           ...node,
+//           children: node.children ? traverse(node.children) : undefined,
+//         });
+//       }
+//     }
+//     return updatedTree;
+//   };
+//   const updatedTree = traverse(tree);
+//   if (linkedNode !== undefined) {
+//     // @ts-ignore
+//     return deleteNodeByUniqueId(updatedTree, linkedNode.id, tree);
+//   }
+//   return updatedTree;
+// };
+// console.log(
+//   "deleteOriginalAndRelatedNodeById",
+//   JSON.stringify(deleteOriginalAndRelatedNodeById(tree, 11), null, 2)
+// );
 const findNodeById = (tree, id) => {
     for (const node of tree) {
         if (node.id === id) {
-            return Object.assign({}, node);
+            return node;
         }
         if (node.children) {
-            const matchInChildren = (0, exports.findNodeById)(node.children, id);
-            if (matchInChildren)
-                return matchInChildren;
+            const match = (0, exports.findNodeById)(node.children, id);
+            if (match)
+                return match;
         }
     }
     return undefined;
 };
 exports.findNodeById = findNodeById;
-// console.log("findNodeById", findNodeById(tree, 311));
-const deleteNodeByUniqueId = (tree, id, originalTree) => {
+// console.log("findNodeById", findNodeById(tree, 31));
+const deleteNodeById = (tree, id) => {
+    let updatedTree = [];
+    for (const node of tree) {
+        if (node.id === id) {
+            continue;
+        }
+        else {
+            updatedTree.push(Object.assign(Object.assign({}, node), { children: node.children ? (0, exports.deleteNodeById)(node.children, id) : undefined }));
+        }
+    }
+    return updatedTree;
+};
+exports.deleteNodeById = deleteNodeById;
+// console.log(
+//   "deleteNodeById",
+//   JSON.stringify(deleteNodeById(tree, 121), null, 2)
+// );
+const deleteNodeByUniqueId = (tree, originalTree, id) => {
     let updatedTree = [];
     for (const node of tree) {
         if (node.id === id) {
@@ -30,7 +163,7 @@ const deleteNodeByUniqueId = (tree, id, originalTree) => {
         }
         else {
             updatedTree.push(Object.assign(Object.assign({}, node), { children: node.children
-                    ? (0, exports.deleteNodeByUniqueId)(node.children, id, originalTree)
+                    ? (0, exports.deleteNodeByUniqueId)(node.children, originalTree, id)
                     : undefined }));
         }
     }
@@ -39,25 +172,55 @@ const deleteNodeByUniqueId = (tree, id, originalTree) => {
 exports.deleteNodeByUniqueId = deleteNodeByUniqueId;
 // console.log(
 //   "deleteNodeByUniqueId",
-//   JSON.stringify(deleteNodeByUniqueId(tree, 3, tree), null, 2)
+//   JSON.stringify(deleteNodeByUniqueId(tree, tree, 2), null, 2)
+// );
+const deleteNodeAndLinkedNodeById = (tree, id) => {
+    let linkedNode = undefined;
+    const traverse = (nodes, id) => {
+        let result = [];
+        for (const node of nodes) {
+            if (node.id === id) {
+                linkedNode = node.linkedCategory
+                    ? (0, exports.findNodeById)(tree, node.linkedCategory)
+                    : undefined;
+            }
+            else if (node.linkedCategory === id) {
+                linkedNode = (0, exports.findNodeById)(tree, node.id);
+            }
+            else {
+                result.push(Object.assign(Object.assign({}, node), { children: node.children ? traverse(node.children, id) : undefined }));
+            }
+        }
+        return result;
+    };
+    const updatedTree = traverse(tree, id);
+    if (linkedNode) {
+        return (0, exports.deleteNodeById)(updatedTree, linkedNode.id);
+    }
+    return updatedTree;
+};
+exports.deleteNodeAndLinkedNodeById = deleteNodeAndLinkedNodeById;
+// console.log(
+//   "deleteNodeAndLinkedNodeById",
+//   JSON.stringify(deleteNodeAndLinkedNodeById(tree, 121), null, 2)
 // );
 const flattenTree = (tree, parentId) => {
-    let flatTree = [];
+    let flatTreet = [];
     for (const node of tree) {
         const newNode = Object.assign({}, node);
-        newNode.parentId = parentId || -1;
         delete newNode.children;
-        flatTree.push(newNode);
+        newNode.parentId = parentId || -1;
+        flatTreet.push(newNode);
         if (node.children) {
-            const flattenChildren = (0, exports.flattenTree)(node.children, node.id);
-            flatTree = [...flatTree, ...flattenChildren];
+            const flatChildren = (0, exports.flattenTree)(node.children, node.id);
+            flatTreet = [...flatTreet, ...flatChildren];
         }
     }
-    return flatTree;
+    return flatTreet;
 };
 exports.flattenTree = flattenTree;
 // console.log("flattenTree", flattenTree(tree));
-const rebuildFlatTree = (flatTree) => {
+const revertFlatTree = (flatTree) => {
     const traverse = (parentId) => {
         let tree = [];
         for (const node of flatTree) {
@@ -74,33 +237,5 @@ const rebuildFlatTree = (flatTree) => {
     };
     return traverse(-1);
 };
-exports.rebuildFlatTree = rebuildFlatTree;
-// console.log(
-//   "rebuildFlatTree",
-//   JSON.stringify(rebuildFlatTree(flattenTree(tree)), null, 2)
-// );
-const deleteOriginalAndRelatedNodeById = (tree, id) => {
-    let linkedNode = undefined;
-    const traverse = (nodes) => {
-        let updatedTree = [];
-        for (const node of nodes) {
-            if (node.id === id) {
-                linkedNode = node.linkedCategory
-                    ? (0, exports.findNodeById)(tree, node.linkedCategory)
-                    : undefined;
-            }
-            else {
-                updatedTree.push(Object.assign(Object.assign({}, node), { children: node.children ? traverse(node.children) : undefined }));
-            }
-        }
-        return updatedTree;
-    };
-    const updatedTree = traverse(tree);
-    if (linkedNode !== undefined) {
-        // @ts-ignore
-        return (0, exports.deleteNodeByUniqueId)(updatedTree, linkedNode.id, tree);
-    }
-    return updatedTree;
-};
-exports.deleteOriginalAndRelatedNodeById = deleteOriginalAndRelatedNodeById;
-console.log("deleteOriginalAndRelatedNodeById", JSON.stringify((0, exports.deleteOriginalAndRelatedNodeById)(consts_1.tree, 11), null, 2));
+exports.revertFlatTree = revertFlatTree;
+console.log("revertFlatTree", JSON.stringify((0, exports.revertFlatTree)((0, exports.flattenTree)(consts_1.tree)), null, 2));
--- src/recursion/practiceFinal.ts
+++ src/recursion/practiceFinal.ts
@@ -1,31 +1,191 @@
 import { tree, type Category } from "./consts";
 
+// export const findNodeById = (
+//   tree: Category[],
+//   id: number
+// ): Category | undefined => {
+//   for (const node of tree) {
+//     if (node.id === id) {
+//       return { ...node };
+//     }
+//     if (node.children) {
+//       const matchInChildren = findNodeById(node.children, id);
+//       if (matchInChildren) return matchInChildren;
+//     }
+//   }
+
+//   return undefined;
+// };
+
+// // console.log("findNodeById", findNodeById(tree, 311));
+
+// export const deleteNodeByUniqueId = (
+//   tree: Category[],
+//   id: number,
+//   originalTree: Category[]
+// ): Category[] => {
+//   let updatedTree: Category[] = [];
+
+//   for (const node of tree) {
+//     if (node.id === id) {
+//       const isUnique = node.linkedCategory
+//         ? findNodeById(originalTree, node.linkedCategory) === undefined
+//         : true;
+
+//       if (!isUnique) {
+//         updatedTree.push({ ...node });
+//       }
+//     } else {
+//       updatedTree.push({
+//         ...node,
+//         children: node.children
+//           ? deleteNodeByUniqueId(node.children, id, originalTree)
+//           : undefined,
+//       });
+//     }
+//   }
+
+//   return updatedTree;
+// };
+
+// // console.log(
+// //   "deleteNodeByUniqueId",
+// //   JSON.stringify(deleteNodeByUniqueId(tree, 3, tree), null, 2)
+// // );
+
+// export const flattenTree = (
+//   tree: Category[],
+//   parentId?: number
+// ): Category[] => {
+//   let flatTree: Category[] = [];
+
+//   for (const node of tree) {
+//     const newNode = { ...node };
+//     newNode.parentId = parentId || -1;
+//     delete newNode.children;
+
+//     flatTree.push(newNode);
+
+//     if (node.children) {
+//       const flattenChildren = flattenTree(node.children, node.id);
+//       flatTree = [...flatTree, ...flattenChildren];
+//     }
+//   }
+
+//   return flatTree;
+// };
+
+// // console.log("flattenTree", flattenTree(tree));
+
+// export const rebuildFlatTree = (flatTree: Category[]): Category[] => {
+//   const traverse = (parentId: number) => {
+//     let tree: Category[] = [];
+//     for (const node of flatTree) {
+//       if (node.parentId === parentId) {
+//         const newNode = { ...node };
+//         delete newNode.parentId;
+//         newNode.children = traverse(node.id).length
+//           ? traverse(node.id)
+//           : undefined;
+//         tree.push(newNode);
+//       }
+//     }
+//     return tree;
+//   };
+
+//   return traverse(-1);
+// };
+
+// // console.log(
+// //   "rebuildFlatTree",
+// //   JSON.stringify(rebuildFlatTree(flattenTree(tree)), null, 2)
+// // );
+
+// export const deleteOriginalAndRelatedNodeById = (
+//   tree: Category[],
+//   id: number
+// ): Category[] => {
+//   let linkedNode: Category | undefined = undefined;
+
+//   const traverse = (nodes: Category[]): Category[] => {
+//     let updatedTree: Category[] = [];
+
+//     for (const node of nodes) {
+//       if (node.id === id) {
+//         linkedNode = node.linkedCategory
+//           ? findNodeById(tree, node.linkedCategory)
+//           : undefined;
+//       } else {
+//         updatedTree.push({
+//           ...node,
+//           children: node.children ? traverse(node.children) : undefined,
+//         });
+//       }
+//     }
+
+//     return updatedTree;
+//   };
+
+//   const updatedTree = traverse(tree);
+
+//   if (linkedNode !== undefined) {
+//     // @ts-ignore
+//     return deleteNodeByUniqueId(updatedTree, linkedNode.id, tree);
+//   }
+
+//   return updatedTree;
+// };
+
+// console.log(
+//   "deleteOriginalAndRelatedNodeById",
+//   JSON.stringify(deleteOriginalAndRelatedNodeById(tree, 11), null, 2)
+// );
+
 export const findNodeById = (
   tree: Category[],
   id: number
 ): Category | undefined => {
   for (const node of tree) {
     if (node.id === id) {
-      return { ...node };
+      return node;
     }
     if (node.children) {
-      const matchInChildren = findNodeById(node.children, id);
-      if (matchInChildren) return matchInChildren;
+      const match = findNodeById(node.children, id);
+      if (match) return match;
     }
   }
-
   return undefined;
 };
 
-// console.log("findNodeById", findNodeById(tree, 311));
+// console.log("findNodeById", findNodeById(tree, 31));
+
+export const deleteNodeById = (tree: Category[], id: number): Category[] => {
+  let updatedTree: Category[] = [];
+
+  for (const node of tree) {
+    if (node.id === id) {
+      continue;
+    } else {
+      updatedTree.push({
+        ...node,
+        children: node.children ? deleteNodeById(node.children, id) : undefined,
+      });
+    }
+  }
+  return updatedTree;
+};
+
+// console.log(
+//   "deleteNodeById",
+//   JSON.stringify(deleteNodeById(tree, 121), null, 2)
+// );
 
 export const deleteNodeByUniqueId = (
   tree: Category[],
-  id: number,
-  originalTree: Category[]
+  originalTree: Category[],
+  id: number
 ): Category[] => {
   let updatedTree: Category[] = [];
-
   for (const node of tree) {
     if (node.id === id) {
       const isUnique = node.linkedCategory
@@ -39,47 +199,84 @@ export const deleteNodeByUniqueId = (
       updatedTree.push({
         ...node,
         children: node.children
-          ? deleteNodeByUniqueId(node.children, id, originalTree)
+          ? deleteNodeByUniqueId(node.children, originalTree, id)
           : undefined,
       });
     }
   }
-
   return updatedTree;
 };
 
 // console.log(
 //   "deleteNodeByUniqueId",
-//   JSON.stringify(deleteNodeByUniqueId(tree, 3, tree), null, 2)
+//   JSON.stringify(deleteNodeByUniqueId(tree, tree, 2), null, 2)
+// );
+
+export const deleteNodeAndLinkedNodeById = (
+  tree: Category[],
+  id: number
+): Category[] => {
+  let linkedNode: Category | undefined = undefined;
+
+  const traverse = (nodes: Category[], id: number): Category[] => {
+    let result: Category[] = [];
+
+    for (const node of nodes) {
+      if (node.id === id) {
+        linkedNode = node.linkedCategory
+          ? findNodeById(tree, node.linkedCategory)
+          : undefined;
+      } else if (node.linkedCategory === id) {
+        linkedNode = findNodeById(tree, node.id);
+      } else {
+        result.push({
+          ...node,
+          children: node.children ? traverse(node.children, id) : undefined,
+        });
+      }
+    }
+    return result;
+  };
+
+  const updatedTree = traverse(tree, id);
+
+  if (linkedNode) {
+    return deleteNodeById(updatedTree, (linkedNode as Category).id);
+  }
+  return updatedTree;
+};
+
+// console.log(
+//   "deleteNodeAndLinkedNodeById",
+//   JSON.stringify(deleteNodeAndLinkedNodeById(tree, 121), null, 2)
 // );
 
 export const flattenTree = (
   tree: Category[],
   parentId?: number
 ): Category[] => {
-  let flatTree: Category[] = [];
+  let flatTreet: Category[] = [];
 
   for (const node of tree) {
     const newNode = { ...node };
-    newNode.parentId = parentId || -1;
     delete newNode.children;
-
-    flatTree.push(newNode);
-
+    newNode.parentId = parentId || -1;
+    flatTreet.push(newNode);
     if (node.children) {
-      const flattenChildren = flattenTree(node.children, node.id);
-      flatTree = [...flatTree, ...flattenChildren];
+      const flatChildren = flattenTree(node.children, node.id);
+      flatTreet = [...flatTreet, ...flatChildren];
     }
   }
 
-  return flatTree;
+  return flatTreet;
 };
 
 // console.log("flattenTree", flattenTree(tree));
 
-export const rebuildFlatTree = (flatTree: Category[]): Category[] => {
-  const traverse = (parentId: number) => {
+export const revertFlatTree = (flatTree: Category[]): Category[] => {
+  const traverse = (parentId: number): Category[] => {
     let tree: Category[] = [];
+
     for (const node of flatTree) {
       if (node.parentId === parentId) {
         const newNode = { ...node };
@@ -90,53 +287,13 @@ export const rebuildFlatTree = (flatTree: Category[]): Category[] => {
         tree.push(newNode);
       }
     }
+
     return tree;
   };
-
   return traverse(-1);
 };
 
-// console.log(
-//   "rebuildFlatTree",
-//   JSON.stringify(rebuildFlatTree(flattenTree(tree)), null, 2)
-// );
-
-export const deleteOriginalAndRelatedNodeById = (
-  tree: Category[],
-  id: number
-): Category[] => {
-  let linkedNode: Category | undefined = undefined;
-
-  const traverse = (nodes: Category[]): Category[] => {
-    let updatedTree: Category[] = [];
-
-    for (const node of nodes) {
-      if (node.id === id) {
-        linkedNode = node.linkedCategory
-          ? findNodeById(tree, node.linkedCategory)
-          : undefined;
-      } else {
-        updatedTree.push({
-          ...node,
-          children: node.children ? traverse(node.children) : undefined,
-        });
-      }
-    }
-
-    return updatedTree;
-  };
-
-  const updatedTree = traverse(tree);
-
-  if (linkedNode !== undefined) {
-    // @ts-ignore
-    return deleteNodeByUniqueId(updatedTree, linkedNode.id, tree);
-  }
-
-  return updatedTree;
-};
-
 console.log(
-  "deleteOriginalAndRelatedNodeById",
-  JSON.stringify(deleteOriginalAndRelatedNodeById(tree, 11), null, 2)
+  "revertFlatTree",
+  JSON.stringify(revertFlatTree(flattenTree(tree)), null, 2)
 );
